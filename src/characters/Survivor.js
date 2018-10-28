f5974193858d0bd52ddf7a43c79b42740df4542f
Núria <soriano.nuria@gmail.com>
2018-10-28 12:16:13 +0100

    add survivor and basic movement

---
+++ src/characters/Survivor.js
@@ -0,0 +1,44 @@
+export default class Survivor extends Phaser.GameObjects.Sprite {
+  constructor(config) {
+    super(config.scene, config.x, config.y, config.key)
+    config.scene.physics.world.enable(this)
+    config.scene.add.existing(this)
+
+    this.target = null
+    this.body.setSize(80, 100)
+    this.body.setCollideWorldBounds(true)
+    this.body.stopVelocityOnCollide = true
+    this.alive = true
+    this.acceleration = 600
+    this.type = 'survivor'
+  }
+
+  setDestination(target) {
+    this.target = target - 40
+  }
+
+  stop() {
+    this.body.velocity.x = 0
+    this.target = null
+  }
+
+  update() {
+    if (this.target) {
+      const movingLeft = this.body.velocity.x < 0
+      const movingRight = this.body.velocity.x > 0
+
+      if (
+        (movingRight && this.body.x >= this.target) ||
+        (movingLeft && this.body.x <= this.target)
+      ) {
+        this.stop()
+      }
+    }
+  }
+
+  die() {
+    this.body.setAcceleration(0)
+    this.body.setVelocity(0, -300)
+    this.alive = false
+  }
+}
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,9 +1,36 @@
+import Survivor from '../characters/Survivor'
+
 class GameScene extends Phaser.Scene {
-    constructor() {
-        super({
-            key: 'GameScene'
-        });
-    }
+  constructor() {
+    super({
+      key: 'GameScene'
+    })
+  }
+
+  preload() {
+    this.load.spritesheet('survivor', 'assets/images/survivor.png', {
+      frameWidth: 80,
+      frameHeight: 100
+    })
+  }
+
+  create() {
+    this.survivor = new Survivor({
+      scene: this,
+      key: 'survivor',
+      x: 40,
+      y: 0
+    })
+
+    this.input.on('pointerdown', pointer => {
+      this.survivor.setDestination(pointer.downX)
+      this.physics.moveTo(this.survivor, pointer.downX, this.survivor.y, 100)
+    })
+  }
+
+  update() {
+    this.survivor.update()
+  }
 }
 
-export default GameScene;
+export default GameScene
