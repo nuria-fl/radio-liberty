6bb447d6d6cb2dfef9f23aacd9f97c7912954959
Núria <soriano.nuria@gmail.com>
2018-11-11 12:40:38 +0100

    add look interaction

--- src/scenes/RoadScene.ts
+++ src/scenes/RoadScene.ts
@@ -7,8 +7,19 @@ class RoadScene extends Phaser.Scene {
   survivor: Survivor
   buggy: Buggy
   floor: Physics.Arcade.Image
-  platforms
+  roadsign: Physics.Arcade.Image
   playingCutscene = true
+
+  look = {
+    sign: {
+      key: 'lookSign',
+      cb: this.lookAtSign
+    },
+    buggy: {
+      key: 'lookBuggy',
+      cb: this.lookAtBuggy
+    }
+  }
 
   constructor() {
     super({
@@ -21,9 +32,14 @@ class RoadScene extends Phaser.Scene {
       scene: this,
       key: IMAGES.BUGGY.KEY,
       x: 1000,
-      y: 300
+      y: 359
     })
     this.physics.add.collider(this.floor, this.buggy)
+
+    this.buggy.setInteractive()
+    this.buggy.on('pointerup', () => {
+      this.sys.events.on(this.look.buggy.key, this.look.buggy.cb, this)
+    })
 
     this.buggy.play('buggy-driving')
 
@@ -47,9 +63,16 @@ class RoadScene extends Phaser.Scene {
       scene: this,
       key: IMAGES.SURVIVOR.KEY,
       x: 100,
-      y: 200
+      y: 366
     })
     this.physics.add.collider(this.floor, this.survivor)
+    this.physics.add.overlap(this.survivor, this.roadsign, () => {
+      this.sys.events.emit(this.look.sign.key)
+    })
+
+    this.physics.add.overlap(this.survivor, this.buggy, () => {
+      this.sys.events.emit(this.look.buggy.key)
+    })
 
     this.input.on('pointerdown', pointer => {
       if (this.playingCutscene === false) {
@@ -59,6 +82,16 @@ class RoadScene extends Phaser.Scene {
     })
   }
 
+  lookAtSign() {
+    console.log('It reads something like… P A … S')
+    this.sys.events.off(this.look.sign.key, this.look.sign.cb, this, false)
+  }
+
+  lookAtBuggy() {
+    console.log('Hmm…')
+    this.sys.events.off(this.look.buggy.key, this.look.buggy.cb, this, false)
+  }
+
   preload() {
     this.load.image(
       IMAGES.ROADSIGN.KEY,
@@ -66,12 +99,16 @@ class RoadScene extends Phaser.Scene {
     )
     this.load.image(IMAGES.ROAD.KEY, `assets/images/${IMAGES.ROAD.FILE}`)
     this.load.image(IMAGES.FLOOR.KEY, `assets/images/${IMAGES.FLOOR.FILE}`)
+    this.load.image(
+      IMAGES.ROADSIGN.KEY,
+      `assets/images/${IMAGES.ROADSIGN.FILE}`
+    )
     this.load.spritesheet(
       IMAGES.BUGGY.KEY,
       `assets/images/${IMAGES.BUGGY.FILE}`,
       {
-        frameWidth: 389,
-        frameHeight: 208
+        frameWidth: 194,
+        frameHeight: 104
       }
     )
 
@@ -79,8 +116,8 @@ class RoadScene extends Phaser.Scene {
       IMAGES.SURVIVOR.KEY,
       `assets/images/${IMAGES.SURVIVOR.FILE}`,
       {
-        frameWidth: 58,
-        frameHeight: 137
+        frameWidth: 37,
+        frameHeight: 88
       }
     )
   }
@@ -93,6 +130,15 @@ class RoadScene extends Phaser.Scene {
       .staticImage(0, 412, IMAGES.FLOOR.KEY)
       .setOrigin(0, 0)
       .refreshBody()
+
+    this.roadsign = this.physics.add
+      .staticImage(660, 340, IMAGES.ROADSIGN.KEY)
+      .refreshBody()
+      .setInteractive()
+
+    this.roadsign.on('pointerup', () => {
+      this.sys.events.on(this.look.sign.key, this.look.sign.cb, this)
+    })
 
     this.initCutscene()
   }
