db3acd4df8ae409163bfe55dbc3e4d77e7c99fec
Nuria <nuria@codegram.com>
2020-05-01 11:05:44 +0200

    change images folder structure

--- src/phaser/constants.ts
+++ src/phaser/constants.ts
@@ -9,55 +9,55 @@ export const SCENES = {
 export const SPRITES = {
   SURVIVOR: {
     KEY: 'SURVIVOR',
-    FILE: 'survivor.png',
+    FILE: 'common/survivor.png',
     WIDTH: 132,
     HEIGHT: 120,
   },
-  STRANGER: {
-    KEY: 'STRANGER',
-    FILE: 'stranger.png',
-    WIDTH: 32,
-    HEIGHT: 100,
-  },
   BUGGY: {
     KEY: 'BUGGY',
-    FILE: 'buggy.png',
+    FILE: 'common/buggy.png',
     WIDTH: 208,
     HEIGHT: 108,
   },
+  STRANGER: {
+    KEY: 'STRANGER',
+    FILE: 'building/stranger.png',
+    WIDTH: 32,
+    HEIGHT: 100,
+  },
   FIREPIT: {
     KEY: 'FIREPIT',
-    FILE: 'fireplace.png',
+    FILE: 'building/fireplace.png',
     WIDTH: 84,
     HEIGHT: 60,
   },
   LADDER: {
     KEY: 'LADDER',
-    FILE: 'ladder.png',
+    FILE: 'building/ladder.png',
     WIDTH: 56,
     HEIGHT: 304,
   },
   BOXES: {
     KEY: 'BOXES',
-    FILE: 'boxes.png',
+    FILE: 'building/boxes.png',
     WIDTH: 128,
     HEIGHT: 148,
   },
   BUCKET: {
     KEY: 'BUCKET',
-    FILE: 'bucket.png',
+    FILE: 'building/bucket.png',
     WIDTH: 28,
     HEIGHT: 48,
   },
   ANTENNA: {
     KEY: 'ANTENNA',
-    FILE: 'antenna.png',
+    FILE: 'building/antenna.png',
     WIDTH: 160,
     HEIGHT: 504,
   },
   METALBOX: {
     KEY: 'METALBOX',
-    FILE: 'metal-box.png',
+    FILE: 'building/metal-box.png',
     WIDTH: 92,
     HEIGHT: 52,
   },
@@ -66,64 +66,64 @@ export const SPRITES = {
 export const IMAGES = {
   PLACEHOLDER: {
     KEY: 'PLACEHOLDER',
-    FILE: 'placeholder.png',
+    FILE: 'common/placeholder.png',
   },
   FLOOR: {
     KEY: 'FLOOR',
-    FILE: 'floor.png',
+    FILE: 'common/floor.png',
   },
   ROADSIGN: {
     KEY: 'ROADSIGN',
-    FILE: 'road-sign.png',
+    FILE: 'road/road-sign.png',
   },
   ROAD: {
     KEY: 'ROAD',
-    FILE: 'road.png',
+    FILE: 'road/road-bg.png',
   },
   ROAD_LONG: {
     KEY: 'ROAD_LONG',
-    FILE: 'road-long.png',
+    FILE: 'road/road-long.png',
   },
   PINECONE: {
     KEY: 'PINECONE',
-    FILE: 'pine-cone.png',
+    FILE: 'road/pine-cone.png',
   },
   BUILDING_BG: {
     KEY: 'BUILDING_BG',
-    FILE: 'building.png',
+    FILE: 'building/building.png',
   },
   BUILDING_BG_2: {
     KEY: 'BUILDING_BG_1',
-    FILE: 'building-layer2.png',
+    FILE: 'building/building-layer2.png',
   },
   BUILDING_NIGHT_BG: {
     KEY: 'BUILDING_NIGHT_BG',
-    FILE: 'building-night.png',
+    FILE: 'building/building-night.png',
   },
   BUILDING_NIGHT_BG_2: {
     KEY: 'BUILDING_NIGHT_BG_1',
-    FILE: 'building-night-layer2.png',
+    FILE: 'building/building-night-layer2.png',
   },
 
   WOOD: {
     KEY: 'WOOD',
-    FILE: 'wood.png',
+    FILE: 'building/wood.png',
   },
   CLOTH: {
     KEY: 'CLOTH',
-    FILE: 'cloth.png',
+    FILE: 'building/cloth.png',
   },
   DROP: {
     KEY: 'DROP',
-    FILE: 'drop.png',
+    FILE: 'building/drop.png',
   },
   ROCK: {
     KEY: 'ROCK',
-    FILE: 'rock.png',
+    FILE: 'building/rock.png',
   },
   BOTTLE: {
     KEY: 'BOTTLE',
-    FILE: 'bottle.png',
+    FILE: 'building/bottle.png',
   },
 }
 
